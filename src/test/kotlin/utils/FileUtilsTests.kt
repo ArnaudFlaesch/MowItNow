4caee65afc3097c6a575d5a5f0375af324858556
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-06-18 22:04:38 +0200

    Fix tests

--- src/test/kotlin/utils/FileUtilsTests.kt
+++ src/test/kotlin/utils/FileUtilsTests.kt
@@ -28,26 +28,26 @@ class FileUtilsTests {
         val testFile = File(this::class.java.classLoader.getResource("mowitnow.txt").file)
         val lineList = parseFileData(testFile)
         assertEquals("5 5", lineList[0])
-        assertEquals("1 2", lineList[1])
-        assertEquals("NGAGAGAGAA", lineList[2])
-        assertEquals("3 3", lineList[3])
-        assertEquals("EAADAADADDA", lineList[4])
+        assertEquals("1 2 N", lineList[1])
+        assertEquals("GAGAGAGAA", lineList[2])
+        assertEquals("3 3 E", lineList[3])
+        assertEquals("AADAADADDA", lineList[4])
     }
 
     @Test
     fun testParseLawnMowerData() {
-        val dataList = listOf("1 2", "NGAGAGAGAA", "3 3", "EAADAADADDA")
+        val dataList = listOf("1 2 N", "GAGAGAGAA", "3 3 E", "AADAADADDA")
         val lawnMowerList = parseLawnMowersData(dataList)
         assertEquals(2, lawnMowerList.size)
 
         assertEquals(1, lawnMowerList[0].coordX)
         assertEquals(2, lawnMowerList[0].coordY)
-        assertEquals(10, lawnMowerList[0].actionList.size)
-        assertTrue(lawnMowerList[0].actionList.map { it.toString() }.containsAll(listOf("N", "G", "A", "G", "A", "G", "A", "G", "A", "A")))
+        assertEquals(9, lawnMowerList[0].actionList.size)
+        assertTrue(lawnMowerList[0].actionList.map { it.toString() }.containsAll(listOf("G", "A", "G", "A", "G", "A", "G", "A", "A")))
 
         assertEquals(3, lawnMowerList[1].coordX)
         assertEquals(3, lawnMowerList[1].coordY)
-        assertEquals(11, lawnMowerList[1].actionList.size)
-        assertTrue(lawnMowerList[1].actionList.map { it.toString() }.containsAll(listOf("E", "A", "A", "D", "A", "A", "D", "A", "D", "D", "A")))
+        assertEquals(10, lawnMowerList[1].actionList.size)
+        assertTrue(lawnMowerList[1].actionList.map { it.toString() }.containsAll(listOf("A", "A", "D", "A", "A", "D", "A", "D", "D", "A")))
     }
 }
