49c369161551ffe88ed6eafd27ecd69294e02c6d
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-06-19 20:32:34 +0200

    Ajout de test

--- src/test/kotlin/SimulatorTests.kt
+++ src/test/kotlin/SimulatorTests.kt
@@ -38,4 +38,21 @@ class SimulatorTests {
         assertEquals(0, updatedLawnMowers[1].coordY)
         assertEquals(DirectionEnum.W, updatedLawnMowers[1].direction)
     }
+
+    @Test
+    fun testSimulatorPreventOutOfBounds() {
+        val lawnMowerList = mutableListOf(
+            LawnMower(0, 0, "DAAAAA".toList(), DirectionEnum.S),
+            LawnMower(5, 0, "GAAAAA".toList(), DirectionEnum.S)
+        )
+        val simulator = Simulator(5, 5, lawnMowerList)
+        val updatedLawnMowers = simulator.initSimulator()
+        assertEquals(0, updatedLawnMowers[0].coordX)
+        assertEquals(0, updatedLawnMowers[0].coordY)
+        assertEquals(DirectionEnum.W, updatedLawnMowers[0].direction)
+
+        assertEquals(5, updatedLawnMowers[1].coordX)
+        assertEquals(0, updatedLawnMowers[1].coordY)
+        assertEquals(DirectionEnum.E, updatedLawnMowers[1].direction)
+    }
 }
