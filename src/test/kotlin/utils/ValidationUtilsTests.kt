d2a3715e09fa7a90bd32da72032bca0e66e2d2be
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-06-19 22:35:25 +0200

    Refacto

--- src/test/kotlin/utils/ValidationUtilsTests.kt
+++ src/test/kotlin/utils/ValidationUtilsTests.kt
@@ -17,54 +17,42 @@ class ValidationUtilsTests {
     @Test
     fun testValidateFileErrorNoData() {
         val testFile = File(this::class.java.classLoader.getResource("mowitnow-empty.txt").file)
-        val exception = assertThrows<Exception>({ "Devrait échouer à cause de l'absence de données dans le fichier" }) {
-            validateFileData(testFile)
-        }
+        val exception = assertThrows<Exception> { validateFileData(testFile) }
         assertEquals("Les données du fichier sont incomplètes.", exception.message)
     }
 
     @Test
     fun testValidateFileErrorMissingMapData() {
         val testFile = File(this::class.java.classLoader.getResource("mowitnow-missingMapData.txt").file)
-        val exception = assertThrows<Exception>({ "Devrait échouer à cause de l'absence de données dans le fichier" }) {
-            validateFileData(testFile)
-        }
+        val exception = assertThrows<Exception> { validateFileData(testFile) }
         assertEquals("Les données de la carte sont invalides.", exception.message)
     }
 
     @Test
     fun testValidateFileErrorWrongMapData() {
         val mapData = listOf("5")
-        val exception = assertThrows<Exception>({ "Devrait échouer à cause de l'absence de données dans le fichier" }) {
-            validateMapData(mapData)
-        }
+        val exception = assertThrows<Exception> { validateMapData(mapData) }
         assertEquals("Les données de la carte sont incomplètes.", exception.message)
     }
 
     @Test
     fun testValidateLawnMowerDataFailMissingActions() {
         val lawnMowersData = listOf("1 2", "GAGAGAGAA", "3 3 E")
-        val exception = assertThrows<Exception>({ "Devrait échouer à cause de l'absence de données dans le fichier" }) {
-            validateLawnMowersData(lawnMowersData)
-        }
+        val exception = assertThrows<Exception> { validateLawnMowersData(lawnMowersData) }
         assertEquals("Les données des tondeuses sont invalides.", exception.message)
     }
 
     @Test
     fun testValidateFileErrorWrongLawnMowerData() {
         val lawnMowersData = listOf("1 2", "GAGAGAGAA", "3 3 E", "GAGAGAGAA")
-        val exception = assertThrows<Exception>({ "Devrait échouer à cause de l'absence de données dans le fichier" }) {
-            validateLawnMowersData(lawnMowersData)
-        }
+        val exception = assertThrows<Exception> { validateLawnMowersData(lawnMowersData) }
         assertEquals("Les données de la tondeuse 0 sont invalides.", exception.message)
     }
 
     @Test
     fun testValidateFileErrorWrongLawnMowerActions() {
         val lawnMowersData = listOf("1 2 N", "GAGAGAGAA", "3 3 E", "EEEE")
-        val exception = assertThrows<Exception>({ "Devrait échouer à cause de l'absence de données dans le fichier" }) {
-            validateLawnMowersData(lawnMowersData)
-        }
+        val exception = assertThrows<Exception> { validateLawnMowersData(lawnMowersData) }
         assertEquals("L'action 0 de la tondeuse 1 est invalide.", exception.message)
     }
 }
